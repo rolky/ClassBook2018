af3e9583778e1f57ca1228c18ed08579958d799c
Vincent Aduuna Frimpong <tossia13@gmail.com>
2018-07-04 19:47:05 +0100

    alligned images

--- UG/generate_html.py
+++ UG/generate_html.py
@@ -24,14 +24,22 @@ def get_images_list():
 	
 def get_html_of_images(images_list):
 	html = ""
-	tag = "<img src={} class='image-fluid'>\n"
+	tag = "<div class='col col-sm-4'><img src=\"{}\" class='img' height=200px width=200px></div>\n"
 	for image in images_list:
 		html += tag.format(image)
 	return html
+	
+	
+def build_bootstrap_html(html_body):
+	with open("base.html",'r') as base:
+		base_html = base.read()
+	bootstrap_html = base_html.format(html_body)
+	return bootstrap_html
 
 if __name__ == "__main__":
 	images_list = get_images_list()
-	html = get_html_of_images(images_list)
+	html_body = get_html_of_images(images_list)
+	html = build_bootstrap_html(html_body)
 	f = open('index.html','w')
 	f.write(html)
 	f.close()
