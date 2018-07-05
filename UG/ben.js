3a26dfd536df2889eca785c63215ab244b9d4890
alexanderweeks <-malextweeks@yahoo.co.uk>
2018-07-05 07:00:07 +0000

    Fancy Bootstrap

--- UG/ben.js
+++ UG/ben.js
@@ -1,6 +1,10 @@
 
 function hello() {
 	alert("Hello World");
+}	
+
+function updateHeader(bananas) {
+	document.getElementById("headerA").innerHTML = bananas
 }
 
 console.log("JS functions loaded");
